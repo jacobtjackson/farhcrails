4dedf18d63985cfd0e7ccd37d9530e61bfe51565
jacobtjackson <jacob.t.jackson@gmail.com>
2016-10-19 16:09:45 -0500

    add necessary changes to prep for dokku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -83,8 +83,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'hidden-hamlet-38592.herokuapp.com' }
-  config.action_mailer.asset_host = 'hidden-hamlet-38592.herokuapp.com'
-  config.assets.digest = true
-  config.action_controller.asset_host = 'hidden-hamlet-38592.herokuapp.com'
 end
